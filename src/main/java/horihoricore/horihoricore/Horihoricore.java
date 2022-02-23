19e92c081fd61fd0f50d85e2c5e7177842371a4a
H4RuB4RE <kasyount@gmail.com>
2022-02-23 20:41:21 +0900

    Summary
    first commit

---
+++ src/main/java/horihoricore/horihoricore/Horihoricore.java
@@ -0,0 +1,61 @@
+package horihoricore.horihoricore;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public final class Horihoricore extends JavaPlugin implements Listener{
+
+    public static String Prefix = "[HoriCore] ";
+    private Listeners listeners;
+
+    public HashMap<Player, Integer> map = new HashMap<>();
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("掘り掘りCoreの起動を開始します...");
+        this.listeners = new Listeners(map);
+        Bukkit.getPluginManager().registerEvents(this.listeners, this);
+        Bukkit.getPluginManager().registerEvents(this, this);
+
+    }
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        map.put(e.getPlayer(), 0);
+    }
+
+    @EventHandler
+    public void onCmd(PlayerCommandPreprocessEvent e) {
+        int value = map.get(e.getPlayer());
+        map.replace(e.getPlayer(), value + 1);
+    }
+
+
+    @Override
+    public void onDisable() {getLogger().info("掘り掘りぱーぼーを終了します"); }
+
+    private static final ItemStack itemStack = new ItemStack(Material.WOODEN_PICKAXE, 1);
+    static {
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        assert itemMeta != null;
+        itemMeta.setDisplayName("初期ピッケル");
+        List<String> Lores = new ArrayList<>();
+        Lores.add("初期に配布されるピッケルです");
+        itemMeta.setLore(Lores);
+        itemStack.setItemMeta(itemMeta);
+    }
+
+
+}
---
+++ src/main/java/horihoricore/horihoricore/Listeners.java
@@ -0,0 +1,52 @@
+package horihoricore.horihoricore;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.util.HashMap;
+
+import static horihoricore.horihoricore.Horihoricore.Prefix;
+
+public class Listeners implements Listener {
+    private final HashMap<Player, Integer> map;
+
+    public Listeners(HashMap<Player, Integer> map) {
+        this.map = map;
+    }
+
+    @EventHandler
+    public void onBreakCobbleStone(BlockBreakEvent e) {
+        if (e.getBlock().getType() == Material.COBBLESTONE)
+            if (e.getPlayer().getWorld().getName().equals("Horihori")) {
+                int value = map.get(e.getPlayer());
+                map.replace(e.getPlayer(), value + 1);
+                e.getPlayer().sendActionBar(ChatColor.YELLOW + "現在の採掘量" + map.get(e.getPlayer()));
+            }
+        if (e.getBlock().getType() == Material.STONE)
+            if (e.getPlayer().getWorld().getName().equals("Horihori")) {
+                int value = map.get(e.getPlayer());
+                map.replace(e.getPlayer(), value + 2);
+                e.getPlayer().sendActionBar(ChatColor.YELLOW + "現在の採掘量" + map.get(e.getPlayer()));
+            }
+        if (map.containsKey(e.getPlayer()) == map.containsValue(100))
+        {
+            e.getPlayer().sendMessage(Prefix + ChatColor.YELLOW + "採掘量が" + ChatColor.RED + "100" + ChatColor.YELLOW + "を超えました！");
+            e.getPlayer().playSound();
+        }
+
+
+    }
+    @EventHandler
+    public boolean onJoin(PlayerJoinEvent e) {
+        if (e.getPlayer().getWorld().getName().equals("Horihori")){
+            if (e.getPlayer().hasPlayedBefore())
+                e.getPlayer().sendMessage(ChatColor.MAGIC.AQUA + "ll" + ChatColor.YELLOW.BOLD + "初回ログイン得点を配布しました！" + ChatColor.MAGIC.AQUA + "ll");
+        }
+        return false;
+    }
+}
